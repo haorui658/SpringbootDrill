0008557cb66c16d25871a79bcdc09d7c189065ed
haorui658 <haorui658@gmail.com>
2017-04-04 16:32:01 +0800

    初始化提交

---
+++ SpringbootDrill/src/main/java/com/jd/risktest/SpringbootDrillApplication.java
@@ -0,0 +1,12 @@
+package com.jd.risktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDrillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDrillApplication.class, args);
+	}
+}
